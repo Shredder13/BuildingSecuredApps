1482167a6b8c5069c0edad043a0fdcd8f8607693
Shredder13 <nofar.schnider@post.idc.ac.il>
2015-12-30 18:50:59 +0200

    probably finished encrypt

--- src/Encrypter.java
+++ src/Encrypter.java
@@ -2,13 +2,13 @@ import javax.crypto.Cipher;
 import javax.crypto.CipherOutputStream;
 import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.nio.file.NoSuchFileException;
+import java.io.*;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.security.*;
 import java.security.cert.Certificate;
+import java.util.Properties;
 
 import javax.crypto.spec.IvParameterSpec;
 
@@ -17,39 +17,48 @@ import javax.crypto.spec.IvParameterSpec;
  */
 public class Encrypter {
 
-    private KeyGenerator kg;
+    private String encryptAlgo = "AES";
+    private String algoMode = "CBC";
+    private String cryptoProvider = "SunJCE";
+    private String padding = "PKCS5Padding";
+    private String signatureAlgo = "SHA1withRSA";
+    private String keyEncryptAlgo = "RSA";
+    private String cipherAlgoMode = "AES/CBC/PKCS5Padding";
+    private String ivAlgo = "SHA1PRNG";
+    private String ivProvider = "SUN";
+
     private KeyStore keystore;
-    private String keystorePass;
     private Certificate certificate;
+    private Signature signature;
 
 
-    public Encrypter(String cryptoProvider, String keystoreName, String keystorePass, String keyAlias, String certAlias) {
+
+    public Encrypter() {
+
+        setConfigurations();
         try {
             keystore = KeyStore.getInstance("JCEKS", cryptoProvider);
-            //TODO: get certificate from keystore
-            certificate = keystore.getCertificate(certAlias);
         } catch (Exception e) {
             //TODO: treat the exception
             e.getMessage();
         }
     }
-    public byte[] encrypt(Path path, String encryptAlgo, String algoMode, String cryptoProvider, String padding, String keystoreName, String keystorePass,
-                          String keyAlias, String keyAliasPass, String keyEncryptAlgo, String signAlgo) {
+    public void encrypt(Path path, String keystoreName, String keystorePass, String certAlias,
+                          String keyAlias, String keyAliasPass) {
         byte[] res = null;
         try {
-            String ciphInit = encryptAlgo + '/' + algoMode + '/' + padding;
-            Cipher ciph = Cipher.getInstance(ciphInit);
 
             // generate a key for the encryption
-            kg = KeyGenerator.getInstance(encryptAlgo, cryptoProvider);
+            KeyGenerator kg = KeyGenerator.getInstance(encryptAlgo, cryptoProvider);
             SecretKey sk = kg.generateKey();
 
             // generate random IV for encryption
-            SecureRandom sr = SecureRandom.getInstance(encryptAlgo, cryptoProvider);
-            byte ivArr[] = new byte[16];
+            SecureRandom sr = SecureRandom.getInstance(ivAlgo, ivProvider);
+            byte[] ivArr = new byte[16];
             sr.nextBytes(ivArr);
             IvParameterSpec iv = new IvParameterSpec(ivArr);
 
+            // load private key and public key from keystore using the given name and password
             keystore.load(new FileInputStream(keystoreName), keystorePass.toCharArray());
             Key ek = keystore.getKey(keyAlias, keyAliasPass.toCharArray());
             PrivateKey priKey = null;
@@ -60,30 +69,43 @@ public class Encrypter {
                     //TODO
                 }
             }
+            // get public key from certificate
             PublicKey pubKey = null;
+            certificate = keystore.getCertificate(certAlias);
             if (certificate != null) {
                 pubKey = certificate.getPublicKey();
             } else {
                 //TODO
             }
 
+            // encryption to the encription algorithm key
+            Cipher ciphKey = Cipher.getInstance(keyEncryptAlgo, cryptoProvider);
+            ciphKey.init(Cipher.ENCRYPT_MODE, pubKey);
+            byte[] encKey = ciphKey.doFinal(sk.getEncoded());
+
+            signature = initSignature(signatureAlgo, "SunRsaSign", priKey);
+
             // initialize cipher for plaintext encryption
+            String ciphInit = encryptAlgo + '/' + algoMode + '/' + padding;
+            Cipher ciph = Cipher.getInstance(ciphInit);
             ciph.init(Cipher.ENCRYPT_MODE, sk, iv);
+
 
             FileInputStream fis = new FileInputStream(path.toString());
             CipherOutputStream cos = new CipherOutputStream(new FileOutputStream("encripted"), ciph);
-            Signature signature = sign(signAlgo, cryptoProvider, priKey);
-            readAndEncrypt(fis, cos, signature);
-            ciph = Cipher.getInstance(keyEncryptAlgo, cryptoProvider);
-            byte[] encKey = ciph.doFinal(sk.getEncoded());
+            readAndEncrypt(fis, cos);
+
+            byte[] signedSig = signature.sign();
+
+            writeToConfig(encKey, signedSig, ivArr);
 
         } catch (Exception e) {
+            //TODO
             System.out.println(e.getMessage());
         }
-        return res;
     }
 
-    private void readAndEncrypt(FileInputStream fis, CipherOutputStream cos, Signature signature) {
+    private void readAndEncrypt(FileInputStream fis, CipherOutputStream cos) {
         byte[] buff = new byte[1024];
         try {
             int num = fis.read(buff);
@@ -96,32 +118,63 @@ public class Encrypter {
             cos.close();
 
         } catch (Exception e) {
-
+            //TODO
         }
     }
 
-    private Signature sign(String signatureAlgo, String cryptoProv, PrivateKey key) {
+    private Signature initSignature(String signatureAlgo, String cryptoProv, PrivateKey key) {
         Signature signa = null;
         try {
             signa = Signature.getInstance(signatureAlgo, cryptoProv);
             signa.initSign(key);
         } catch (Exception e) {
             // TODO: handle
+            e.getMessage();
         }
         return signa;
     }
 
-    public void setKeystore(String keystoreName) {
+    private void setConfigurations() {
+        File file = new File("config.ini");
         try {
-            this.keystore.load(new FileInputStream(keystoreName), keystorePass.toCharArray());
-        } catch (Exception e) {
-            //TODO: treat the exception
-            e.getMessage();
+            FileReader reader = new FileReader(file);
+            Properties properties = new Properties();
+            properties.load(reader);
+            encryptAlgo = properties.getProperty("encrypt_algorithm");
+            algoMode = properties.getProperty("encrypt_algorithm_mode");
+            cryptoProvider = properties.getProperty("crypto_provider");
+            padding = properties.getProperty("padding");
+            signatureAlgo = properties.getProperty("signature_algorithm");
+            keyEncryptAlgo = properties.getProperty("key_encrypt_algorithm");
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
         }
-
     }
 
-    public void setKeystorePass(String password) {
-        keystorePass = password;
+    private void writeToConfig(byte[] encriptedKey, byte[] signature, byte[] iv) {
+        try {
+            Path path = Paths.get("encrypted_output");
+            String line = "encryption_algorithm=" + encryptAlgo + "\n";
+            Files.write(path, line.getBytes());
+            line = "signature_algorithm=" + signatureAlgo + "\n";
+            Files.write(path, line.getBytes());
+            line = "key_encription_algorithm=" + keyEncryptAlgo + "\n";
+            Files.write(path, line.getBytes());
+            line = "cipher_algorithm_mode=" + cipherAlgoMode + "\n";
+            Files.write(path, line.getBytes());
+            line = "encripted_key=" + "\n";
+            Files.write(path, line.getBytes());
+            Files.write(path, encriptedKey);
+            line = "file_signature=" + "\n";
+            Files.write(path, line.getBytes());
+            Files.write(path, signature);
+            line = "iv=" + "\n";
+            Files.write(path, line.getBytes());
+            Files.write(path, iv);
+        } catch (Exception e) {
+            // TODO
+        }
+
+
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,3 @@
-
-
-//import javax.crypto.CipherOutputStream;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
@@ -9,7 +6,7 @@ public class Main {
 
     public static void main(String[] args) {
         if (args.length != 6) {
-            System.out.println("Usage: EnryptAndSign.jar <File Path> <Keystore name> <Keystore Password> <Key Alias> <Key Password> <Certificate Alias>");
+            System.out.println("Usage: EncryptAndSign.jar <File Path> <Keystore name> <Keystore Password> <Key Alias> <Key Password> <Certificate Alias>");
             return;
         } else {
             Path path = Paths.get(args[0]);
@@ -18,20 +15,8 @@ public class Main {
             String keyAlias = args[3];
             String keyAliasPass = args[4];
             String certAlias = args[5];
-            EncryptAndSign eas = new EncryptAndSign(path, keyStoreName, keyStorePass, keyAlias, keyAliasPass, certAlias);
-
-            // AES with CBC and random IV
-            // the key for the AES we randomly select using a function from the API
-
-            //eas.sign();
+            Encrypter encrypter = new Encrypter();
+            encrypter.encrypt(path, keyStoreName, keyStorePass, certAlias, keyAlias, keyAliasPass);
         }
-        // 1. get plaintext
-        // 2. encode and write cypher
-        // 3. calculate digital signature (a-symetric) of the file's content and save to config file
-        // the config file can contain the encrypted key
-
-        // IMPORTANT:
-
-        // have to be able to switch the algorithm and crypto providers easily -> config file!
     }
 }
